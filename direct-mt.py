a5b7e58d9ed4851bbb46158e6778c416fed40c20
Simon Dae-oong Kim <daeoong.kim@gmail.com>
2013-03-06 14:52:44 -0800

    much better matchin dictionary translation. it now prints sentence by sentence in list. need to do pos tagging now.

--- direct-mt.py
+++ direct-mt.py
@@ -11,78 +11,75 @@ class MT:
 
     def __init__(self, text):
 
-        # Reads the text in and translate word by word into English
         for line in open (text, "r"):
-            print line
-            translation = ""
             words = line.split()
-            for word in words:
+            if len(words) > 1:
+                self.dictionary[words[0]] = words[1]
+            else:
+                self.dictionary[words[0]] = ""
 
-                while len(word) > 0:
-
-                    # data = urllib2.urlopen('http://www.google.com/dictionary/json?callback=dict_api.callbacks.id100&q=test&sl=en&tl=en&restrict=pr%2Cde&client=te')
-                    #
-                    # j = json.load(data)
-                    # k = [i for i, j, k in j[1]]
-                    # l = json.dumps(k)
-                    # print l
-                    #
-                    # exit()
-
-                    # print word
-                    search_url="http://api.wordreference.com/386fd/koen/"+word
-                    # print search_url
-                    results=urllib.urlopen(search_url)
-                    # exit()
-
-                    content=results.read()
-                    # print content
-                    definitions = re.findall(r'<span class=\'FrW2\'>([ a-zA-Z]*)</span>', content, re.MULTILINE)
-                    if not len(definitions) == 0:
-                        # print definitions
-                        self.dictionary[word] = definitions
-                        translation += definitions[0] + " "
-                        break
-
-                    word = word[:-1]
-                    # print word
-            print translation
+        for paragraph in open ('text.txt', "r"):
+            lines = paragraph.split('. ')
+            for line in lines:
+                trans_line = []
+                words = line.split()
+                if not len(words) == 0:
+                    for word in words:
+                        if self.dictionary.has_key(word):
+                            if not len(self.dictionary[word]) == 0:
+                                trans_line.append(self.dictionary[word])
+                    # trans_line.append(".")
+                    print trans_line
 
 
-
-            # for l in results.readlines():
-                #     try:
-                #         # print l
-                #         m = re.search(r'<span class=\'FrW2\'>(.*)</span>', l)
-                #         print m
-                #
-                #         # # Search the returned HTMl for the hyper-link data for the speciic player.
-                #         # # This is mostly string clean up stiff to make the URL string ready for thte next step.
-                #         #     split1=l.split('=')
-                #         #     first_piece=split1[1].lstrip('"')
-                #         #     second_piece=split1[2].split('"')[0]
-                #         #     dictionary[word]="http://www.nfl.com"+first_piece+"="+second_piece
-                #     except UnicodeDecodeError:
-                #         print "Ignoring UnicodeDecodeError"
-                # results.close()
-                #
-                # exit()
-            # print words
-
-
-            # print line
-        #     = open (text, "r")
-        # data = f.read()
-        #
-        # for line in data:
+        # output = ""
+        # # Reads the text in and translate word by word into English
+        # for line in open (text, "r"):
         #     print line
-            # words = line.split()
-            # for word in words:
-            #     print word
-
+        #     translation = ""
+        #     words = line.split()
+        #     for word in words:
+        #         output += (word + '\n')
+        # print output
+        # f = open('output.txt', 'w+')
+        # f.write(output)
+        # exit()
+            #
+            #     while len(word) > 0:
+            #
+            #         # data = urllib2.urlopen('http://www.google.com/dictionary/json?callback=dict_api.callbacks.id100&q=test&sl=en&tl=en&restrict=pr%2Cde&client=te')
+            #         #
+            #         # j = json.load(data)
+            #         # k = [i for i, j, k in j[1]]
+            #         # l = json.dumps(k)
+            #         # print l
+            #         #
+            #         # exit()
+            #
+            #         # print word
+            #         search_url="http://endic.naver.com/popManager.nhn?m=search&query="+word
+            #         # print search_url
+            #         results=urllib.urlopen(search_url)
+            #         # exit()
+            #
+            #         content=results.read()
+            #         # print content
+            #         definitions = re.findall(r'<a href="popManager\.nhn\?m=search&query=[a-zA-Z]*">([ a-zA-Z]*)</a>', content, re.MULTILINE)
+            #         # print definitions
+            #         # exit()
+            #
+            #         if not len(definitions) == 0:
+            #             print definitions
+            #             self.dictionary[word] = definitions
+            #             translation += definitions[0] + " "
+            #             break
+            #
+            #         word = word[:-1]
+            #         # print word
+            # print translation
 
 
 if __name__ == '__main__':
-    text = 'text.txt'
+    text = 'output.txt'
 
     result = MT(text)
